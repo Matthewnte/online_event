aad97479a16ce7447541cfea46e050373de9de88
Matthew Nte <Matthewnte1@gmail.com>
2020-09-21 05:29:27 +0100

    add eventRoute

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 
+// Import routes
+const eventRoute = require('./routes/event');
+
 // initialize express server
 const app = express();
+
+// parses incomming request to json object
+app.use(express.json());
+
+// entry to event route
+app.use('/api/v1/events', eventRoute);
 
 module.exports = app;
