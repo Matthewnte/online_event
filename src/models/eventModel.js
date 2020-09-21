974a72d2dead18f55675a62cd5ab21e81c9e4bed
Matthew Nte <Matthewnte1@gmail.com>
2020-09-21 05:32:18 +0100

    modified event model

--- src/models/eventModel.js
+++ src/models/eventModel.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
 
+const categories = ['sport', 'social', 'business', 'tech'];
+
 const eventSchema = mongoose.Schema({
-  name: { type: String, required: 'Event must have a name', trim: true },
-  event: { type: String, required: 'Event must have venue' },
+  name: {
+    type: String,
+    required: 'Event must have a name',
+    trim: true,
+    unique: true,
+  },
+  category: {
+    type: String,
+    required: 'Event must have a category',
+    enum: categories,
+  },
+  platform: { type: String, required: 'Event must have venue' },
   image: { type: String, required: 'Event must have an image' },
   description: { type: String, trim: true },
   ratingAverage: { type: Number, default: 4.5 },
   ratingQuantity: { type: Number, default: 0 },
-  eventStartTime: { type: Date, required: 'Event must have a start time' },
+  startDate: { type: Date, required: 'Event must have a start Date' },
   price: Number,
+  maxNumberOfAttendees: { type: Number },
   status: { type: Boolean, default: true },
 });
 
