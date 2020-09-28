a510411e71e21b38206e74c1942402fbe07f590c
Matthew Nte <Matthewnte1@gmail.com>
2020-09-28 05:38:42 +0100

    add async error handler

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -1,100 +1,82 @@
 const Event = require('../models/eventModel');
 const ApiFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+const catchAsyncError = require('../utils/catchAsyncError');
 
-exports.createEvent = async (req, res) => {
-  try {
-    const reqBody = { ...req.body, category: req.body.category.toLowerCase() };
-    const newEvent = await Event.create(reqBody);
-    return res.status(201).json({
-      status: 'success',
-      data: {
-        event: newEvent,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      status: 'fail',
-      error,
-    });
+exports.createEvent = catchAsyncError(async (req, res) => {
+  const reqBody = { ...req.body, category: req.body.category.toLowerCase() };
+  const newEvent = await Event.create(reqBody);
+  return res.status(201).json({
+    status: 'success',
+    data: {
+      event: newEvent,
+    },
+  });
+});
+
+exports.getAllEvents = catchAsyncError(async (req, res) => {
+  // Execute query
+  const features = new ApiFeatures(Event.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const events = await features.query;
+
+  // Send response
+  return res.status(200).json({
+    status: 'success',
+    results: events.length,
+    data: {
+      events,
+    },
+  });
+});
+
+exports.getEvent = catchAsyncError(async (req, res, next) => {
+  const event = await Event.findById(req.params.id);
+
+  if (!event) {
+    return next(new AppError('No event found', 404));
   }
-};
 
-exports.getAllEvents = async (req, res) => {
-  try {
-    // Execute query
-    const features = new ApiFeatures(Event.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const events = await features.query;
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      event,
+    },
+  });
+});
 
-    // Send response
-    return res.status(200).json({
-      status: 'success',
-      results: events.length,
-      data: {
-        events,
-      },
-    });
-  } catch (error) {
-    return res.status(404).json({
-      status: 'fail',
-      message: error,
-    });
+exports.updateEvent = catchAsyncError(async (req, res, next) => {
+  const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!event) {
+    return next(new AppError('No event found', 404));
   }
-};
 
-exports.getEvent = async (req, res) => {
-  try {
-    const tour = await Event.findById(req.params.id);
-    return res.status(200).json({
-      status: 'success',
-      data: {
-        tour,
-      },
-    });
-  } catch (error) {
-    return res.status(404).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      event,
+    },
+  });
+});
 
-exports.updateEvent = async (req, res) => {
-  try {
-    const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    return res.status(200).json({
-      status: 'success',
-      data: {
-        event,
-      },
-    });
-  } catch (error) {
-    return res.status(404).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+exports.deleteEvent = catchAsyncError(async (req, res, next) => {
+  const event = await Event.findByIdAndDelete(req.params.id);
 
-exports.deleteEvent = async (req, res) => {
-  try {
-    await Event.findByIdAndDelete(req.params.id);
-    return res.status(204).json({
-      status: 'success',
-      data: {
-        event: null,
-      },
-    });
-  } catch (error) {
-    return res.status(404).json({
-      status: 'fail',
-      message: error,
-    });
+  if (!event) {
+    return next(new AppError('No event found', 404));
   }
-};
+
+  return res.status(204).json({
+    status: 'success',
+    data: {
+      event: null,
+    },
+  });
+});
