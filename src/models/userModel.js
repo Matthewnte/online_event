2733211e665194a13ce95c4635c089946d4d710b
Matthew Nte <Matthewnte1@gmail.com>
2020-10-04 06:13:28 +0100

    src/models/userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -34,6 +34,11 @@ const userSchema = mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
