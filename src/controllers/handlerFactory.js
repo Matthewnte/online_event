c8fa0df7da8162fb549aa3881726cf0d4c1efb73
Matthew Nte <Matthewnte1@gmail.com>
2020-10-10 01:09:29 +0100

    ft(handler-factory):dotenv[Finishes #49]

--- src/controllers/handlerFactory.js
+++ src/controllers/handlerFactory.js
@@ -1,7 +1,6 @@
 const catchAsyncError = require('../utils/catchAsyncError');
 const AppError = require('../utils/appError');
 const ApiFeatures = require('../utils/apiFeatures');
-const { query } = require('express');
 
 exports.deleteOne = (Model) => catchAsyncError(async (req, res, next) => {
   const doc = await Model.findByIdAndDelete(req.params.id);
