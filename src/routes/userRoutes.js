4199bb95baca9b712b26c6fbb72c2639caa713db
Matthew Nte <Matthewnte1@gmail.com>
2020-10-11 12:23:34 +0100

    ft(set-general-auth)[#57]

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -14,30 +14,25 @@ router.post('/refresh-token', authController.refreshToken);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+// Protect all routes after this meddleware
+router.use(authMiddleware.authCheck);
+
 router.patch(
   '/updatePassword',
   authMiddleware.authCheck,
   authController.updatePassword,
 );
 
-router.get('/me', authMiddleware.authCheck, userController.getMe, userController.getUser);
-router.patch('/updateMe', authMiddleware.authCheck, userController.updateMe);
-router.delete('/deleteMe', authMiddleware.authCheck, userController.deleteMe);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
-router.route('/').get(authMiddleware.authCheck, userController.getAllUsers);
+router.route('/').get(userController.getAllUsers);
 
 router
   .route('/:id')
-  .get(authMiddleware.authCheck, userController.getUser)
-  .patch(
-    authMiddleware.authCheck,
-    authMiddleware.restrictTo('admin'),
-    userController.updateUser,
-  )
-  .delete(
-    authMiddleware.authCheck,
-    authMiddleware.restrictTo('admin'),
-    userController.deleteUser,
-  );
+  .get(userController.getUser)
+  .patch(authMiddleware.restrictTo('admin'), userController.updateUser)
+  .delete(authMiddleware.restrictTo('admin'), userController.deleteUser);
 
 module.exports = router;
