78d59a7ba0d4a7ef28b6fc77a6869d446e6d4f6c
Matthew Nte <Matthewnte1@gmail.com>
2020-10-14 01:06:47 +0100

    ft(handle-app-error)[finishes #22]

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+// Handle Uncaught Exception
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./src/app');
 const config = require('./src/config');
 
@@ -5,4 +12,13 @@ const config = require('./src/config');
 require('./src/db/mongoose');
 
 // listen for request on port
-app.listen(config.port, () => console.log(`Server listening on port: ${config.port}`));
+const server = app.listen(config.port, () => console.log(`Server listening on port: ${config.port}`));
+
+// Handle unhandled rejection
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
